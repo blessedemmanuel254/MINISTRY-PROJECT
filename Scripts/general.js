77ed4d63ed0528ff4209f40ce777ac01ce6be635
Emmanuel Werangai <emmanueltindi23@gmail.com>
2025-08-21 17:56:33 +0300

    commit

--- Scripts/general.js
+++ Scripts/general.js
@@ -5,3 +5,11 @@ function toggleSideBar() {
   popupbar.classList.toggle('active');
   overlay.classList.toggle('active');
 }
+
+/* Code Popup Overlay */
+function toggleCodePopup() {
+  const codOverlay = document.getElementById('codOverlay');
+  const codePrompt = document.getElementById('codePrompt');
+  codOverlay.classList.toggle('active');
+  codePrompt.classList.toggle('active');
+}
